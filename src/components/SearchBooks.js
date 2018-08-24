5739c9c59ac7a54ab35ff031aea75565a968fed9
juan odiccio <juanodicio@gmail.com>
2018-08-23 23:08:21 -0700

    Added Links to navigate between / and /search

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,5 +1,5 @@
-import { Component } from 'react'
-import React from "react";
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchBooks extends Component {
 
@@ -7,7 +7,8 @@ class SearchBooks extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link className="close-search" to="/">Close</Link>
+
           <div className="search-books-input-wrapper">
             {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
