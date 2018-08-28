fa31440e7ee5915a9c7cab29ddf8d1e1bf231e94
juan odiccio <juanodicio@gmail.com>
2018-08-28 00:39:33 -0700

    Refactoring searchBooks

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -11,11 +11,8 @@ class SearchBooks extends Component {
     moveBook: PropTypes.func.isRequired
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: []
-    }
+  state = {
+    results: []
   }
 
   search = debounce((term) => {
@@ -52,14 +49,6 @@ class SearchBooks extends Component {
           <Link className="close-search" to="/">Close</Link>
 
           <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
             <input type="text" onKeyUp={ this.onSearch }
                    placeholder="Search by title or author"/>
 
